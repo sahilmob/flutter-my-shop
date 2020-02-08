397082f38e4ba5693189c292039d2655cd7a4b8c
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-02-08 17:25:53 +0200

    get product details from provider

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -40,4 +40,8 @@ class Products with ChangeNotifier {
   List<Product> get items {
     return [..._items];
   }
+
+  Product findById(String id) {
+    return _items.firstWhere((item) => item.id == id);
+  }
 }
--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import 'package:provider/provider.dart';
+import "../providers/products.dart";
 
 class ProductDetailsScreen extends StatelessWidget {
   static const routeName = "/product-details";
@@ -6,9 +8,13 @@ class ProductDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
+    final product = Provider.of<Products>(
+      context,
+      listen: false,
+    ).findById(productId);
     return Scaffold(
       appBar: AppBar(
-        title: Text(productId),
+        title: Text(product.title),
       ),
     );
   }
