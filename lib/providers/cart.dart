410f8fd10bc5113e9f95df35c94380e7e1799bd6
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-03-07 18:55:26 +0200

    add total amount getter

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -25,6 +25,12 @@ class Cart with ChangeNotifier {
     return _items.length;
   }
 
+  double get totalAmount {
+    var total = 0.0;
+    _items.forEach((key, item) => total += item.price * item.quantity);
+    return total;
+  }
+
   void addItem(String productId, double price, String title) {
     if (items.containsKey(productId)) {
       _items.update(
