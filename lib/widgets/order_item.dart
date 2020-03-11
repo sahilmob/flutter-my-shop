8fe584a733a937ced6112c8c9ee144598b817e2f
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-03-11 21:34:43 +0200

    create order item

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,13 +1,23 @@
+import 'dart:math';
 import "package:flutter/material.dart";
 import "package:intl/intl.dart";
 
 import 'package:my_shop/providers/orders.dart' as orderProvider;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final orderProvider.OrderItem order;
 
-  const OrderItem({Key key, this.order}) : super(key: key);
+  OrderItem({
+    Key key,
+    this.order,
+  }) : super(key: key);
 
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  bool _expanded = false;
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -15,15 +25,55 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: <Widget>[
           ListTile(
-            title: Text("\$${order.amout}"),
+            title: Text("\$${widget.order.amout}"),
             subtitle: Text(
-              DateFormat("dd/MM/yyyy hh:mm").format(order.dateTime),
+              DateFormat("dd/MM/yyyy hh:mm").format(widget.order.dateTime),
             ),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             ),
-          )
+          ),
+          if (_expanded)
+            Container(
+              padding: EdgeInsets.symmetric(
+                horizontal: 15,
+                vertical: 4,
+              ),
+              height: min(
+                widget.order.products.length * 20.0 + 10,
+                100,
+              ),
+              child: ListView(
+                children: widget.order.products
+                    .map(
+                      (product) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            product.title,
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            "${product.quantity}x \$${product.price}",
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.grey,
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                    .toList(),
+              ),
+            )
         ],
       ),
     );
