4cc9b16e1c1e692e3715a8512c87f879f0162860
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-03-11 19:52:52 +0200

    create order item widget

---
+++ lib/widgets/order_item.dart
@@ -0,0 +1,31 @@
+import "package:flutter/material.dart";
+import "package:intl/intl.dart";
+
+import 'package:my_shop/providers/orders.dart' as orderProvider;
+
+class OrderItem extends StatelessWidget {
+  final orderProvider.OrderItem order;
+
+  const OrderItem({Key key, this.order}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10),
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            title: Text("\$${order.amout}"),
+            subtitle: Text(
+              DateFormat("dd MM yyyy hh:mm").format(order.dateTime),
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.expand_more),
+              onPressed: () {},
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
