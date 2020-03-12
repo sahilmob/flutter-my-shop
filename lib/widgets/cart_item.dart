54a0ec322b89bbabf3a4ad0cb38b380f1510bd72
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-03-12 21:20:00 +0200

    show dialog to confirm cart item removal

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -36,6 +36,27 @@ class CartItem extends StatelessWidget {
         margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
       ),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Text("Are you sure?"),
+            content: Text("Do you want to remove the item from the cart?"),
+            actions: <Widget>[
+              FlatButton(
+                  onPressed: () {
+                    Navigator.of(ctx).pop(false);
+                  },
+                  child: Text("No")),
+              FlatButton(
+                  onPressed: () {
+                    Navigator.of(ctx).pop(true);
+                  },
+                  child: Text("Yes"))
+            ],
+          ),
+        );
+      },
       onDismissed: (_direction) {
         cart.removeItem(cartItemId);
       },
