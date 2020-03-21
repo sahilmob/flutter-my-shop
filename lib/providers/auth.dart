e02fd72cce8cb44736707ebc09fac8f17e2156ba
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-03-21 21:48:37 +0200

    refactor and implement login

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -7,9 +7,10 @@ class Auth with ChangeNotifier, JsonHelpers {
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signup(String email, String password) async {
-    const url =
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyASqz26nzmVdRPVEwf1vQ4BswO4iNqMC9Y";
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyASqz26nzmVdRPVEwf1vQ4BswO4iNqMC9Y";
     final response = await http.post(
       url,
       body: encode(
@@ -17,5 +18,13 @@ class Auth with ChangeNotifier, JsonHelpers {
       ),
     );
     print(decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, "signUp");
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, "signInWithPassword");
   }
 }
