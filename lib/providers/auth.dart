a4bedf6254a738891c7b6184964a8e06e2b4156c
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2020-03-21 22:08:44 +0200

    handling auth errors

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/widgets.dart';
 import "package:http/http.dart" as http;
 import 'package:my_shop/mixins/json_helpers.dart';
+import 'package:my_shop/models/http_exception.dart';
 
 class Auth with ChangeNotifier, JsonHelpers {
   String _token;
@@ -11,13 +12,21 @@ class Auth with ChangeNotifier, JsonHelpers {
       String email, String password, String urlSegment) async {
     final url =
         "https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyASqz26nzmVdRPVEwf1vQ4BswO4iNqMC9Y";
-    final response = await http.post(
-      url,
-      body: encode(
-        {"email": email, "password": password, "returnSecureToken": true},
-      ),
-    );
-    print(decode(response.body));
+    try {
+      final response = await http.post(
+        url,
+        body: encode(
+          {"email": email, "password": password, "returnSecureToken": true},
+        ),
+      );
+      final decodedResponse = decode(response.body);
+      if (decodedResponse["error"] != null) {
+        throw HttpException(decodedResponse["error"]["message"]);
+      }
+      print(decode(decodedResponse));
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> signup(String email, String password) async {
